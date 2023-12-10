b9aaedc49f3d547cfe5ff968a5c38b26e0fc6dc3
Vladyslav2123 <qazewo@gmail.com>
2023-12-10 11:38:27 +0100

    Change ViewModel

--- ApplicationUI/ViewModels/ReservationDateViewModel.cs
+++ ApplicationUI/ViewModels/ReservationDateViewModel.cs
@@ -1,5 +1,31 @@
-﻿namespace ApplicationUI.ViewModels;
+﻿using ApplicationUI.Views;
+using System.Collections.ObjectModel;
+
+namespace ApplicationUI.ViewModels;
 
 public class ReservationDateViewModel : ViewModelBase
 {
+    RoomView roomView;
+
+    public ObservableCollection<RoomView> Rooms { get; set; }
+
+    public RoomView Room
+    {
+        get
+        {
+            return roomView;
+        }
+        set
+        {
+            roomView = value;
+            OnPropertyChanged(nameof(Room));
+        }
+    }
+
+    public ReservationDateViewModel()
+    {
+        roomView = new RoomView();
+        Rooms = new ObservableCollection<RoomView>();
+        Rooms.Add(roomView);
+    }
 }
